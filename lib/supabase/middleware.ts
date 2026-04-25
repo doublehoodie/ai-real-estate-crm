96a0c7d4d00cebad264726e3bbd1b1d0916ed628
Yan Zhou <zhouyanpaul@gmail.com>
2026-04-25 17:08:33 -0400

    Middleware fix

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -31,6 +31,8 @@ export async function updateSession(request: NextRequest) {
     },
   );
 
+  await supabase.auth.getUser();
+
   const {
     data: { user },
   } = await supabase.auth.getUser();
